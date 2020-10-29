acfa69b5d7b5f5ba4e717054999903bc3734b87d
teakguillaume@gmail.com <147896523teak>
2020-10-29 18:58:31 +0100

    App Basic Front done

--- recompositron/main.js
+++ recompositron/main.js
@@ -2,18 +2,73 @@ const electron = require('electron');
 const url = require('url');
 const path = require ('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow; 
 
 // Listen for the App to be ready
 app.on('ready', function(){
     // Create new window 
-    mainWindow =  new BrowserWindow({});
+    mainWindow =  new BrowserWindow({
+        width: 1400,
+        height: 657,
+        title: 'Pollen Analyser' 
+    });
     // Load html into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
         protocol:'file:',
         slashes: true
     }));
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate) ; 
+    //Inser menu 
+    Menu.setApplicationMenu(mainMenu);
+
+    
+
 });
+
+
+// Handle create add window 
+function createAddWindow(){
+     // Create new window 
+    addWindow =  new BrowserWindow({
+        width: 1260,
+        height: 657,
+        title: 'Add Pollen' 
+    });
+    // Load html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol:'file:',
+        slashes: true
+    }));
+}
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu:[
+            {
+                label: 'Add Item'
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 
+                'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]   
+    }
+]
+
+
+
